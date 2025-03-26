a439a0e1485966b82d4ac31d5f64049eefb5ea3f
KaueSiqueira <kaue54150@gmail.com>
2025-03-26 20:03:07 -0300

    atualização

--- JS/script.js
+++ JS/script.js
@@ -14,3 +14,30 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+//Trocando de tema
+
+const toggleTheme = document.getElementById("toggleTheme");
+
+const rootHtml = document.documentElement;
+
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  if (currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
+  else rootHtml.setAttribute("data-theme", "dark");
+
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
